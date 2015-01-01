9781361dd2f39b54c98d19632b0094fe75db1ae9
Matthias Bannert <bannert@kof.ethz.ch>
2015-01-01 00:00:00 +0000

    version 0.1

---
+++ inst/dropR_shiny/www/linechart-binding.js
@@ -0,0 +1,86 @@
+// Put code in an Immediately Invoked Function Expression (IIFE).
+// This isn't strictly necessary, but it's good JavaScript hygiene.
+(function() {
+
+// See http://rstudio.github.io/shiny/tutorial/#building-outputs for
+// more information on creating output bindings.
+
+// First create a generic output binding instance, then overwrite
+// specific methods whose behavior we want to change.
+var binding = new Shiny.OutputBinding();
+
+binding.find = function(scope) {
+  // For the given scope, return the set of elements that belong to
+  // this binding.
+  return $(scope).find(".nvd3-linechart");
+};
+
+binding.renderValue = function(el, data) {
+  // This function will be called every time we receive new output
+  // values for a line chart from Shiny. The "el" argument is the
+  // div for this particular chart.
+  
+  var $el = $(el);
+    
+    
+  
+  // The first time we render a value for a particular element, we
+  // need to initialize the nvd3 line chart and d3 selection. We'll
+  // store these on $el as a data value called "state".
+  if (!$el.data("state")) {
+    var chart = nv.models.lineChart()
+      .margin({left: 100})
+      .useInteractiveGuideline(true)
+      .transitionDuration(350)
+      .showLegend(true)
+      .showYAxis(true)
+      .showXAxis(true)
+      .forceY([0,1])
+      // https://github.com/novus/nvd3/issues/428#issuecomment-41918302 
+      // modifying tooltip when using interactive guideline seems to be quite an issue.. 
+      
+      
+    chart.xAxis     //Chart x-axis settings
+      .axisLabel('question sequence')
+      .tickFormat(d3.format(',r'));
+ 
+    chart.yAxis     //Chart y-axis settings
+      .axisLabel('Percent remaining participants')
+      .tickFormat(d3.format('.02f'));
+      
+    
+    
+
+    nv.utils.windowResize(chart.update);
+    
+    var selection = d3.select(el).select("svg");
+    
+    // Store the chart object on el so we can get it next time
+    $el.data("state", {
+      chart: chart,
+      selection: selection
+    });
+  }
+  
+  
+  
+  // Now, the code that'll run every time a value is rendered...
+  
+  // Retrieve the chart and selection we created earlier
+  var state = $el.data("state");
+  
+  // Schedule some work with nvd3
+  nv.addGraph(function() {
+    // Update the chart
+    state.selection
+      .datum(data)
+      .transition(500)
+      .call(state.chart);
+    return state.chart;
+  });
+};
+
+// Tell Shiny about our new output binding
+Shiny.outputBindings.register(binding, "shinybinding.nvd3-linechart");
+
+})();
